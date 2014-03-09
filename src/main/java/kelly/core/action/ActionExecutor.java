a391010d3744e8ea8adbae9e4237f08cd8e21441
YING Zhuo <yingzhor@163.com>
2014-03-09 15:14:57 +0800

    添加ActionResult接口及其实现

--- src/main/java/kelly/core/action/ActionExecutor.java
+++ src/main/java/kelly/core/action/ActionExecutor.java
@@ -1,6 +1,22 @@
 package kelly.core.action;
 
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import kelly.core.ActionResult;
+import kelly.core.Model;
+import kelly.core.ModelAndView;
+import kelly.core.result.ByteArrayActionResult;
+import kelly.core.result.FileActionResult;
+import kelly.core.result.InputStreamActionResult;
+import kelly.core.result.ModelActionResult;
+import kelly.core.result.ModelAndViewActionResult;
+import kelly.core.result.NullActionResult;
+import kelly.util.ReflectionUtils;
 
 
 /**
@@ -11,7 +27,40 @@ import kelly.core.ActionResult;
  */
 public final class ActionExecutor {
 
-	public ActionResult execute(InvokableAction invokableAction) throws Throwable {
+	public ActionResult execute(InvokableAction invokableAction, Object[] args, HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		Object controllerObject = invokableAction.getControllerObject();
+		Method method = invokableAction.getMethod();
+		
+		Object result = ReflectionUtils.invokeMethod(method, controllerObject, args);
+		
+		if (result == null) {
+			return new NullActionResult(invokableAction, request, response);
+		}
+
+		if (result instanceof ActionResult) {
+			return (ActionResult) result;
+		}
+		
+		if (result instanceof InputStream) {
+			return new InputStreamActionResult((InputStream) result, invokableAction, request, response);
+		}
+		
+		if (result instanceof File) {
+			return new FileActionResult((File) result, invokableAction, request, response);
+		}
+		
+		if (result.getClass() == byte[].class) {
+			return new ByteArrayActionResult((byte[]) result, invokableAction, request, response);
+		}
+		
+		if (result instanceof ModelAndView) {
+			return new ModelAndViewActionResult((ModelAndView) result, invokableAction, request, response);
+		}
+		
+		if (result instanceof Model) {
+			return new ModelActionResult((Model) result, invokableAction, request, response);
+		}
+		
 		return null;
 	}
 
