c2b10fe17920a47f3ca31a7978168d0a24bcf723
YING Zhuo <yingzhor@163.com>
2014-03-10 19:37:50 +0800

    添加Predicate<String>的实现，用户分检对静态资源的请求

---
+++ src/main/java/kelly/core/path/DefaultStaticResourcePredicate.java
@@ -0,0 +1,42 @@
+package kelly.core.path;
+
+import kelly.core.functor.Predicate;
+
+public class DefaultStaticResourcePredicate implements Predicate<String> {
+
+	private static final String[] ANT_STYLE_PATTERNS = new String[] {
+		"/static/**",
+		"/statics/**",
+		"/icon/**",
+		"/icons/**",
+		"/css/**",
+		"/js/**",
+		"/javascript/**",
+		"/images/**",
+		"/image/**",
+		"/swf/**",
+		"/flash/**",
+		"/**/*.css",
+		"/**/*.js",
+		"/**/*.ico",
+		"/**/*.png",
+		"/**/*.gif",
+		"/**/*.jpg",
+		"/**/*.jpeg",
+		"/**/*.bmp"
+	};
+	
+	private static final PathMatcher PATH_MATCHER = new AntStylePathMatcher();
+	
+	// -----------------------------------------------------------------------------------------
+
+	@Override
+	public boolean evaluate(String input) {
+		for (String pattern : ANT_STYLE_PATTERNS) {
+			if (PATH_MATCHER.match(pattern, input)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
