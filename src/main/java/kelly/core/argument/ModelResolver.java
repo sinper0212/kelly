2e5f382cb559434d6cb525da20f3be6c14c26021
YING Zhuo <yingzhor@163.com>
2014-03-11 09:49:17 +0800

    修复一处bug

--- src/main/java/kelly/core/argument/ModelResolver.java
+++ src/main/java/kelly/core/argument/ModelResolver.java
@@ -3,6 +3,7 @@ package kelly.core.argument;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kelly.core.Model;
 import kelly.core.action.ActionArgument;
 import kelly.core.castor.Castor;
 import kelly.core.exception.KellyException;
@@ -14,7 +15,7 @@ public class ModelResolver extends AbstractActionArgumentResolver {
 	@Override
 	public boolean supports(ActionArgument actionArgument, Castor castor, HttpServletRequest request) {
 		Class<?> type = actionArgument.getParameterType();
-		return ClassUtils.isAssignable(type, MapModel.class);
+		return ClassUtils.isAssignable(type, Model.class);
 	}
 
 	@Override
