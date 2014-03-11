6a9db2741364bf02cbe8b1726629416e9b4caf5d
YING Zhuo <yingzhor@163.com>
2014-03-11 11:25:33 +0800

    Spring集成插件微调，支持@Nullable标注

--- src/main/java/kelly/core/annotation/Inject.java
+++ src/main/java/kelly/core/annotation/Inject.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Documented
 public @interface Inject {
 
-	String value();
+	String value() default "";
 
 }
---
+++ src/main/java/kelly/core/injector/AbstractSpringInjector.java
@@ -0,0 +1,93 @@
+package kelly.core.injector;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import kelly.core.annotation.Inject;
+import kelly.core.annotation.Nullable;
+import kelly.core.exception.KellyException;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.ReflectionUtils;
+
+/**
+ * kelly与spring的整合点
+ * 
+ * @author 应卓(yingzhor@gmail.com)
+ *
+ */
+public abstract class AbstractSpringInjector implements Injector {
+
+	private final ApplicationContext applicationContext;
+	
+	protected abstract ApplicationContext getApplicationContext();
+
+	public AbstractSpringInjector() {
+		this.applicationContext = getApplicationContext();
+	}
+	
+	@Override
+	public final void inject(Object bean) {
+		if (bean == null) return;
+		
+		Field[] fields = getFieldsIncludingSuperclass(bean);
+		
+		for (Field field : fields) {
+			Inject inject = field.getAnnotation(Inject.class);
+			boolean nullable = field.getAnnotation(Nullable.class) != null;
+			if (inject == null) {
+				continue;
+			} else {
+				
+				String beanName = inject.value();
+				Object com = null;
+				if ("".equals(beanName)) {		// 按类型注入
+					com = getBeanFromApplicationContext(field.getType());
+				} else {
+					com = getBeanFromApplicationContext(beanName);
+				}
+				
+				if (com == null) {
+					if (nullable) {
+						continue;
+					} else {
+						throw new KellyException("Inject failed for field " + field.toString() );
+					}
+				}
+				ReflectionUtils.makeAccessible(field);
+				ReflectionUtils.setField(field, bean, com);
+			}
+		}
+	}
+	
+	// -------------------------------------------------------------------------
+
+	private Field[] getFieldsIncludingSuperclass(Object bean) {
+		Class<?> type = bean.getClass();
+		List<Field> fields = new ArrayList<Field>();
+		while (type != null) {
+			fields.addAll(Arrays.asList(type.getDeclaredFields()));
+			type = type.getSuperclass();
+		}
+		return fields.toArray(new Field[fields.size()]);
+	}
+
+	private Object getBeanFromApplicationContext(Class<?> beanType) {
+		try {
+			return applicationContext.getBean(beanType);
+		} catch (BeansException e) {
+			return null;
+		}
+	}
+	
+	private Object getBeanFromApplicationContext(String beanName) {
+		try {
+			return applicationContext.getBean(beanName);
+		} catch (BeansException e) {
+			return null;
+		}
+	}
+}
